3b997894535a4fa2d24c7f9d8071db618fefbd07
raouf_bou <raoufboukhobza3@gmail.com>
2026-07-26 01:43:01 +0100

    create the tenant context interceptor

---
+++ common/common.module.ts
@@ -0,0 +1,11 @@
+import { Global, Module } from '@nestjs/common';
+import { TenantContextInterceptor } from './tenant-context/tenant-context.interceptor';
+import { PrismaService } from './prisma/prisma.service';
+
+@Global()
+@Module({
+  controllers: [],
+  providers: [TenantContextInterceptor, PrismaService],
+  exports: [TenantContextInterceptor, PrismaService],
+})
+export class CommonModule {}
