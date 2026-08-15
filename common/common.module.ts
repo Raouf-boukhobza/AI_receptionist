fd1c035b16f677ee7499f9b568c26b2a7bf12521
raouf_bou <raoufboukhobza3@gmail.com>
2026-08-15 15:43:15 +0100

    create the redis factory provider

--- common/common.module.ts
+++ common/common.module.ts
@@ -1,11 +1,13 @@
 import { Global, Module } from '@nestjs/common';
 import { TenantContextInterceptor } from './tenant-context/tenant-context.interceptor';
 import { PrismaService } from './prisma/prisma.service';
+import { RedisModule } from './redis/redis.module';
 
 @Global()
 @Module({
+  imports: [RedisModule],
   controllers: [],
   providers: [TenantContextInterceptor, PrismaService],
-  exports: [TenantContextInterceptor, PrismaService],
+  exports: [TenantContextInterceptor, PrismaService, RedisModule],
 })
 export class CommonModule { }
