4b6c55d9a9eabae1ecf938f67d2cc1d9622902b1
raouf_bou <raoufboukhobza3@gmail.com>
2026-08-21 23:32:22 +0100

    create the inboudn queue processor

--- src/modules/messaging/messaging.module.ts
+++ src/modules/messaging/messaging.module.ts
@@ -11,31 +11,63 @@ import { AgentModule } from '../agent/agent.module';
 import { TenantModule } from '../tenantModule/tenant.module';
 import { MessagesService } from './messages.service';
 import { MessagesController } from './messages.controller';
+import {
+  OUTBOUND_MESSAGES_QUEUE,
+  OutboundMessagesQueue,
+} from './queue/outbound-messages.queue';
+import { InboundMessagesProcessor } from './queue/inbound-messages.processor';
+import { ConversationsService } from '../conversations/conversations.service';
+import { ConversationsModule } from '../conversations/conversations.module';
 
 @Module({
   imports: [
     BullmqModule,
     TenantModule,
-    forwardRef(() => AgentModule),
-    BullModule.registerQueue({
-      name: INBOUND_MESSAGES_QUEUE,
-      defaultJobOptions: {
-        attempts: 3,
-        backoff: {
-          type: 'exponential',
-          delay: 2000,
+    ConversationsModule,
+    AgentModule,
+    BullModule.registerQueue(
+      {
+        name: INBOUND_MESSAGES_QUEUE,
+        defaultJobOptions: {
+          attempts: 3,
+          backoff: {
+            type: 'exponential',
+            delay: 2000,
+          },
+          removeOnComplete: { age: 3600, count: 1000 }, // Keep last 1000 for 1 hr
+          removeOnFail: { age: 86400 * 7 }, // Keep failed jobs for 7 days in DLQ
         },
-        removeOnComplete: { age: 3600, count: 1000 }, // Keep last 1000 for 1 hr
-        removeOnFail: { age: 86400 * 7 }, // Keep failed jobs for 7 days in DLQ
       },
-    }),
-    BullBoardModule.forFeature({
-      name: INBOUND_MESSAGES_QUEUE,
-      adapter: BullMQAdapter,
-    }),
+      {
+        name: OUTBOUND_MESSAGES_QUEUE,
+        defaultJobOptions: {
+          attempts: 3,
+          backoff: {
+            type: 'exponential',
+            delay: 2000,
+          },
+          removeOnComplete: { age: 3600, count: 1000 }, // Keep last 1000 for 1 hr
+          removeOnFail: { age: 86400 * 7 }, // Keep failed jobs for 7 days in DLQ
+        },
+      },
+    ),
+    BullBoardModule.forFeature(
+      {
+        name: INBOUND_MESSAGES_QUEUE,
+        adapter: BullMQAdapter,
+      },
+      {
+        name : OUTBOUND_MESSAGES_QUEUE,
+        adapter : BullMQAdapter
+      },
+    ),
   ],
   controllers: [MessagesController],
-  providers: [InboundMessagesQueue, MessagesService],
-  exports: [InboundMessagesQueue, MessagesService],
+  providers: [
+    InboundMessagesQueue,
+    MessagesService,
+    OutboundMessagesQueue,
+    InboundMessagesProcessor,
+  ]
 })
 export class MessagingModule {}
